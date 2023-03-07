445d6bb079bafc35dbca2bfb791e776232ff4cb5
almondamonnut <athammatadatrakoon@gmail.com>
2023-03-07 23:16:45 +0700

    correct the definition of push_back()

--- list.cpp
+++ list.cpp
@@ -7,7 +7,7 @@ int main(){
     list<int> queue;
     cout << queue.empty()  << " "; // 1=true
 
-    queue.push_back(10); // added 1 element at the end: O(1)
+    queue.push_back(10); // added 1 element after it currect last element: O(1)
     cout << queue.empty() << " "; // 0=false
 
     int s = queue.front();
