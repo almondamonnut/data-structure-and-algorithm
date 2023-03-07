33156aa482f19543b466b35552239a2804e09b7e
almondamonnut <athammatadatrakoon@gmail.com>
2023-03-07 23:00:50 +0700

    add comment

--- list.cpp
+++ list.cpp
@@ -1,14 +1,20 @@
+// 
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
     list<int> queue;
-    cout << queue.empty()  << " ";
-    queue.push_back(10);
-    cout << queue.empty() << " ";
+    cout << queue.empty()  << " "; // 1=true
+
+    queue.push_back(10); // added 1 element at the end: O(1)
+    cout << queue.empty() << " "; // 0=false
+
     int s = queue.front();
     cout << s << " ";
     queue.pop_front();
     cout << queue.empty();
+
+    // cout << queue(0)
 
 }
