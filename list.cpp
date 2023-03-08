415f166892557ad1a903dab51e2abb998a4657c5
almondamonnut <athammatadatrakoon@gmail.com>
2023-03-08 13:04:18 +0700

    add

--- list.cpp
+++ list.cpp
@@ -3,11 +3,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
+
     list<int> queue;
     cout << queue.empty()  << " "; // 1=true
 
-    queue.push_back(10); // added 1 element after it currect last element: O(1)
+    queue.push_back(10); // added 1 element after its current last element: O(1)
     cout << queue.empty() << " "; // 0=false
 
     int s = queue.front();
@@ -15,6 +16,5 @@ int main(){
     queue.pop_front();
     cout << queue.empty();
 
-    // cout << queue(0)
-
+    //cout << queue[0] will raise an error. We cannot access element of list using any kind of bracket
 }
