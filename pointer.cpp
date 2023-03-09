7b19b9c5167090fd0656efcc6d29047b4d74301e
almondamonnut <athammatadatrakoon@gmail.com>
2023-03-09 20:06:43 +0700

    pointer study

---
+++ pointer.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// to receive an address, create a pointer here
+void func(string* ptr) {
+    *ptr = "jelly";
+}
+
+int main() {
+    string food = "Pizza";
+    cout << food <<" " << &food << endl;
+
+    string* ptrf = &food;
+    func(ptrf); // to pass an adress, you can pass a pointer
+    cout << food;
+
+    food = "jam";
+    func(&food); // or you can also pass an adress directly
+    cout << "  " << food;
+}
