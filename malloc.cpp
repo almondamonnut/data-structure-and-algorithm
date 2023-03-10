bb8dae8c863a31635e73488551e5ffa9aff35e1f
almondamonnut <athammatadatrakoon@gmail.com>
2023-03-10 22:10:58 +0700

    study malloc intro

---
+++ malloc.cpp
@@ -0,0 +1,26 @@
+// This code is to learn the concept of malloc in c++
+// malloc is to allocate a block of memory to a pointer
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    // allocate memory of int size to an int pointer
+    // malloc(...) returns a pointer to the beginning of the block
+    // with empty value in the block
+    // the original type of malloc(...) is of type void*
+    // (int*) here is to cast the type of void* to int*
+    // thanks info from cplusplus.com
+    int* ptr = (int*) malloc(sizeof(int));
+
+    cout << ptr << endl;
+    cout << *ptr << endl;
+
+    // assign the value 5 to the allocated memory
+    // *ptr means "the value that ptr points to"
+    *ptr = 5;
+
+    cout << ptr << endl;
+    cout << *ptr << endl;
+
+}
