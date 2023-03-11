b9437779c8a0423799c33449fe208dee15222ddd
almondamonnut <athammatadatrakoon@gmail.com>
2023-03-11 21:03:31 +0700

    pause to study iterator first

---
+++ map.cpp
@@ -0,0 +1,18 @@
+// map in c++ standard template library (STL)
+// each element has a key value & map value
+// 2 values cannot have the same key
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // struture: map<type1, type2> name_of_map;
+    map<string, int> map_a;
+
+    // Insert values into the map;
+    map["one"] = 1;
+    map["two"] = 2;
+    map["three"] = 3;
+
+    // get an iterator pointingto the first element in the map
+}
