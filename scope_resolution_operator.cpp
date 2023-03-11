43df56e9538fa15a551efdc484ee110e71aaa4af
almondamonnut <athammatadatrakoon@gmail.com>
2023-03-11 22:34:08 +0700

    3 purposes of scope resolution operator

--- scope_resolution_operator.cpp
+++ scope_resolution_operator.cpp
@@ -7,11 +7,21 @@ using namespace std;
 int x; 
 
 class classA {
+    // static = all object of this class use the same value of variable
+    static int x;
     public:
         void func();
+        void func2(int x) {
+            // ___purpose#3___: access a static variable
+            // of the same name as local variable
+            cout << "static x" << ::x << endl; // but if we also have global of the same name, it's gonna refer to the global one.
+            cout << "local x" << x <<endl;
+        }
 }; // class needs a semicolon to end the declaration
 
-// ___purpos#2: accssing a class's function outside the class
+int classA::x = 300;
+
+// ___purpos#2___: accssing a class's function outside the class
 // in order to define the function of it
 void classA::func() {
     cout << "func() called";
@@ -25,5 +35,9 @@ int main() {
     // where there is a local varaible of the same name
     cout << "global x:" << ::x << endl;
     cout << "local x:" << x << endl; 
+
+    // declare object of classA
+    classA objA;
+    objA.func2(100);
 
 }
