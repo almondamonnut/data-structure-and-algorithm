8a095e696000fbdd5b0ca9337276da716c961397
almondamonnut <athammatadatrakoon@gmail.com>
2023-03-11 21:20:36 +0700

    pause to study :: in c++

---
+++ iterator.cpp
@@ -0,0 +1,13 @@
+// Iterators are in c++ STL (Standard Template Library)
+// Iterators are used to point at the memory addresses
+// of STL containers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> ar = {1, 2, 3, 4, 5};
+    
+    // Declaring iterator to a vector
+    vector<int>::iterator ptr;
+}
