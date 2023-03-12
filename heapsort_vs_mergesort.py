a47ccd2a7fcc20986c648968ba82f0819873862f
almondamonnut <athammatadatrakoon@gmail.com>
2023-03-12 20:49:05 +0700

    comparison between heap sort and merge sort

---
+++ heapsort_vs_mergesort.py
@@ -0,0 +1,126 @@
+# Python program for implementation of heap Sort 
+
+# To heapify subtree rooted at index i. 
+# n is size of heap 
+
+from numpy import random
+import numpy as np
+
+### HEAP SORT
+
+def heapify(arr, n, i): 
+	largest = i # Initialize largest as root 
+	l = 2 * i + 1	 # left = 2*i + 1 
+	r = 2 * i + 2	 # right = 2*i + 2 
+
+	# See if left child of root exists and is 
+	# greater than root 
+	if l < n and arr[i] < arr[l]: 
+		largest = l 
+
+	# See if right child of root exists and is 
+	# greater than root 
+	if r < n and arr[largest] < arr[r]: 
+		largest = r 
+
+	# Change root, if needed 
+	if largest != i: 
+		arr[i],arr[largest] = arr[largest],arr[i] # swap 
+
+		# Heapify the root. 
+		heapify(arr, n, largest) 
+
+# The main function to sort an array of given size 
+def heapSort(arr): 
+	n = len(arr) 
+
+	# Build a maxheap. 
+	for i in range(n, -1, -1): 
+		heapify(arr, n, i) 
+
+	# One by one extract elements 
+	for i in range(n-1, 0, -1): 
+		arr[i], arr[0] = arr[0], arr[i] # swap 
+		heapify(arr, i, 0) 
+		
+
+#### MERGE SORT ---------------------------------------------------------
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+ 
+    # create temp arrays
+    L = [0] * (n1)
+    R = [0] * (n2)
+ 
+    # Copy data to temp arrays L[] and R[]
+    for i in range(0, n1):
+        L[i] = arr[l + i]
+ 
+    for j in range(0, n2):
+        R[j] = arr[m + 1 + j]
+ 
+    # Merge the temp arrays back into arr[l..r]
+    i = 0     # Initial index of first subarray
+    j = 0     # Initial index of second subarray
+    k = l     # Initial index of merged subarray
+ 
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+ 
+    # Copy the remaining elements of L[], if there
+    # are any
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+ 
+    # Copy the remaining elements of R[], if there
+    # are any
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+ 
+# l is for left index and r is right index of the
+# sub-array of arr to be sorted
+ 
+ 
+def mergeSort(arr, l, r):
+    if l < r:
+ 
+        # Same as (l+r)//2, but avoids overflow for
+        # large l and h
+        m = l+(r-l)//2
+ 
+        # Sort first and second halves
+        mergeSort(arr, l, m)
+        mergeSort(arr, m+1, r)
+        merge(arr, l, m, r)
+ 
+ 
+
+
+arr = random.randint(5000000, size=(5000000))
+arr2 = np.copy(arr)
+print(arr)
+import time
+st = time.time()
+heapSort(arr) 
+print(time.time() - st) # 88.91 seconds, 89.90 seconds, 92.07 seconds
+print(arr)
+
+
+st = time.time()
+mergeSort(arr2, 0, len(arr2)-1)
+print("\n\nSorted array is", arr2)
+
+print(time.time() - st) # 39.57 seconds, 39.85 seconds, 42.77 seconds
+
+## conclusion: merge sort is always better than heap sort
